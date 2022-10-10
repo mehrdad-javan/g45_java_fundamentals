4a9d0a70c620b7ce78602c9ae78fe833378694b0
Mehrdad Javan <mehrdad.javan@adapta.se>
2022-10-10 11:08:43 +0200

    arithmetic operators

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,6 +6,33 @@ public class App {
   public static void main(String[] args){
     // statement for printing the data
     System.out.println("Hello World!");
+
+    int age= 32;
+    System.out.println("Age: " + age);
+
+    double salary = 30000.00;
+
+    // sout + tab
+    System.out.println("Salary: " + salary);
+
+    boolean studentStatus = true;
+    boolean accountStatus = false;
+
+    char a= 'a';
+
+    double test;
+    test = 12.22;
+
+    int number1 = 20;
+    int number2 = 10;
+    // arithmetic operators ( + - / * )
+    int result = number1 + number2;
+    System.out.println("Result:" + result);
+
+    int number = 0;
+    //System.out.println(number++);
+    System.out.println(++number);
+
   }
 
 }
