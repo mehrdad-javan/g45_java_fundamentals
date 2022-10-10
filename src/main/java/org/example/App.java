ae98395a59688113b46735e8b51a056d5c70647a
Mehrdad Javan <mehrdad.javan@adapta.se>
2022-10-10 10:32:33 +0200

    add a comment for print statement

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,6 +4,7 @@ public class App {
 
   // step1 define a main method
   public static void main(String[] args){
+    // statement for printing the data
     System.out.println("Hello World!");
   }
 
